c28d878391e9bc86489b21c2027271ec2c34919f
PAVAN903 <navap903@gmail.com>
2023-02-26 22:00:48 +0530

    added:created addding list

--- src/com/bridgelab/Main.java
+++ src/com/bridgelab/Main.java
@@ -1,10 +1,11 @@
 package com.bridgelab;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	static AdressBook adressbook;
-
+    static ArrayList<AdressBook> adressBook=new ArrayList<AdressBook>();
 	public void Uc1_createcontact() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter first name");
@@ -25,11 +26,19 @@ public class Main {
 		adressbook = new AdressBook(fname, lname, city, state, zip, mobile, email);
 
 	}
+	public void Uc2_addingContacts() {
+		adressBook.add(adressbook);
+		System.out.println("contact list added");
+	}
 
 	public static void main(String[] args) {
 		Main object = new Main();
 		object.Uc1_createcontact();
 		System.out.println(adressbook);
+		System.out.println();
+		object.Uc1_createcontact();
+		object.Uc2_addingContacts();
+		System.out.println(adressBook.get(0));
 
 	}
 }
